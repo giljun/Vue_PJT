55679788841881060b67876e9476f6e6e4355f92
giljun <giljune91@gmail.com>
2019-09-30 09:25:38 +0900

    delete pjt

--- README.md
+++
@@ -1,3 +0,0 @@
-# 1주차 프로젝트
- - Web / Mobile with Vue.js, Vuetify
- - 반응형 웹 페이지 제작
--- webmobile/src/components/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <v-footer class="pa-3">
-    <div>&copy; {{ new Date().getFullYear() }} GILJUN PARK</div>
-  </v-footer>
-</template>
