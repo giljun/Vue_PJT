5ad8243f9bb826d1f5432451d5c23764bceb4bb5
giljun <giljune91@gmail.com>
2019-09-30 09:29:53 +0900

    20190930|commit history test

---
+++ webmobile/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <Header />
+    <v-content>
+      <router-view />
+    </v-content>
+    <ScrollBtn />
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import store from "./store";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ScrollBtn from "./components/ScrollBtn";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    store,
+    Footer,
+    ScrollBtn
+  },
+  data() {
+    return {
+      browser: BrowserDetect
+    };
+  },
+  beforeCreate() {
+    if (browser != "Chrome") {
+      alert("해당 사이트는 크롬에 최적화 되어 있습니다.");
+    }
+  }
+};
+</script>
